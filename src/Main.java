7c86640b28ca3fe35b8e8265dc17bfc6a24f382e
Ethan <crowelle550@gmail.com>
2024-10-01 12:48:31 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String name = "";
+        String trash = "";
+        double salary = 0;
+        boolean doneSalary = false;
+
+        do {
+
+            System.out.println("Enter your salary:");
+            if (in.hasNextDouble()) {
+                salary = in.nextDouble();
+                in.nextLine(); // clear buffer
+                System.out.println("You said your salary is " + salary);
+                doneSalary = true;
+            } else {
+                trash = in.nextLine();
+                System.out.println("You entered " + trash);
+                System.out.println("Run the program and try again!");
+            }
+        } while (!doneSalary);
+    }
+}
